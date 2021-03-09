27cb5efdef770f35098103e2e46213867f01bac1
Max Meldrum <max@meldrum.se>
2021-03-09 09:05:24 +0100

    add test_data file with decoder

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use arcon::prelude::*;
+use arcon::arcon_decoder;
+
+#[arcon_decoder(,)]
+#[derive(Arcon, Arrow, prost::Message, Clone)]
+#[arcon(unsafe_ser_id = 12, reliable_ser_id = 13, version = 1, keys = "id")]
+pub struct TestData {
+    #[prost(uint64)]
+    pub id: u64,
+    #[prost(string)]
+    pub msg: String,
+    #[prost(float)]
+    pub f: f32,
+}
 
 fn main() {
+
     let mut pipeline = Pipeline::default()
-        .collection((0..100).collect::<Vec<u64>>(), |conf| {
+        .file("test_data", |conf| {
             conf.set_arcon_time(ArconTime::Event);
-            conf.set_timestamp_extractor(|x: &u64| *x);
+            conf.set_timestamp_extractor(|x: &TestData| x.id);
         })
         .operator(OperatorBuilder {
-            constructor: Arc::new(|_| Filter::new(|x| *x > 50)),
+            constructor: Arc::new(|_| Filter::new(|x: &TestData| x.f > 2.5)),
             conf: Default::default(),
         })
         .to_console()
