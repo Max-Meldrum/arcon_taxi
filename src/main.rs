1615f18500fee41374ff8ea74457e35704dcc440
Klas Segeljakt <klasseg@kth.se>
2021-03-09 14:40:49 +0100

    Add documentation for data fields

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,65 @@ use chrono::NaiveDateTime;
 #[derive(Arcon, Arrow, Clone)]
 #[arcon(unsafe_ser_id = 12, reliable_ser_id = 13, version = 1, keys = "vendor_id")]
 pub struct TaxiRideData {
+    /// A code indicating the TPEP provider that provided the record.
+    /// 1 = Creative Mobile Technologies, LLC; 2 = VeriFone Inc,
     pub vendor_id: u64,
+    /// The date and time when the meter was engaged.
     pub tpep_pickup_datetime: String,
+    /// The date and time when the meter was disengaged.
     pub tpep_dropoff_datetime: String,
+    /// The number of passengers in the vehicle. This is a driver-entered value.
     pub passenger_count: u64,
+    /// The elapsed trip distance in miles reported by the taximeter.
     pub trip_distance: f32,
+    /// The final rate code in effect at the end of the trip.
+    ///
+    /// * 1 = Standard rate
+    /// * 2 = JFK
+    /// * 3 = Newark
+    /// * 4 = Nassau or Westchester
+    /// * 5 = Negotiated fare
+    /// * 6 = Group ride
     pub rate_code_id: u64,
+    /// This flag indicates whether the trip record was held in vehicle
+    /// memory before sending to the vendor, aka “store and forward,”
+    /// because the vehicle did not have a connection to the server.
+    ///
+    /// * Y = store and forward trip
+    /// * N = not a store and forward trip
     pub store_and_fwd_flag: String,
+    /// TLC Taxi Zone in which the taximeter was engaged
     pub pu_location_id: u64,
+    /// TLC Taxi Zone in which the taximeter was disengaged
     pub du_location_id: u64,
+    /// A numeric code signifying how the passenger paid for the trip.
+    ///
+    /// * 1 = Credit card
+    /// * 2 = Cash
+    /// * 3 = No charge
+    /// * 4 = Dispute
+    /// * 5 = Unknown
+    /// * 6 = Voided trip
     pub payment_type: u64,
+    /// The time-and-distance fare calculated by the meter.
     pub fare_amount: u64,
+    /// Miscellaneous extras and surcharges. Currently, this only includes
+    /// the $0.50 and $1 rush hour and overnight charges
     pub extra: u64,
+    /// $0.50 MTA tax that is automatically triggered based on the metered
+    /// rate in use.
     pub mta_tax: f32,
+    /// Tip amount – This field is automatically populated for credit card
+    /// tips. Cash tips are not included.
     pub tip_amount: f32,
+    /// Total amount of all tolls paid in trip.
     pub tolls_amount: u64,
+    /// $0.30 improvement surcharge assessed trips at the flag drop. The
+    /// improvement surcharge began being levied in 2015
     pub improvement_surcharge: f32,
+    /// The total amount charged to passengers. Does not include cash tips.
     pub total_amount: f32,
+    /// This field is not documented.
     pub congestion_surcharge: f32,
 }
 
