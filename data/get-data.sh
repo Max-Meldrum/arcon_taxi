121ee45e660137ed2c61b8d2452d0042f3ac241c
Klas Segeljakt <klasseg@kth.se>
2021-03-12 15:54:35 +0100

    Update get-data.sh again, delete all lines which do not contain 2020

--- data/get-data.sh
+++ data/get-data.sh
@@ -3,6 +3,6 @@
 wget -i ./data_url.md
 
 /bin/cat yellow_tripdata_2020-*\.csv \
-  | sed '/2021/d' \
+  | sed '/2020-[0-9][0-9]-[0-9][0-9]/!d'
   | sort -t, -k 3n,3 \
   >> sorted_yellow_tripdata_2020.csv
